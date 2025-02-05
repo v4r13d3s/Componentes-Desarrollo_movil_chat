575ebde881a852ba816053452b2ed9087f8c407e
v4r13d3s <20233L001041@utcv.edu.mx>
2025-02-05 05:33:59 -0600

    Actualización - implementación de capacitor

---
+++ android/app/src/main/java/com/appIonic/WhatsApp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.appIonic.WhatsApp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.appIonic.WhatsApp',
   appName: 'Chat_Whats',
   webDir: 'www'
 };
